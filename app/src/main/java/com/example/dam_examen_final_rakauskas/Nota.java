19bbfab1ae66a4b27ef7cd99304aeb484561d9b1
Sebastian Rakauskas <sebastianalberto258@hotmail.com>
2023-07-17 14:20:34 -0500

    Login

---
+++ app/src/main/java/com/example/dam_examen_final_rakauskas/Nota.java
@@ -0,0 +1,8 @@
+package com.example.dam_examen_final_rakauskas;
+
+public class Nota {
+    private String titulo;
+    private String contenido;
+    private boolean favorita;
+    private int color;
+}
